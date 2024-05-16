8929c33db864d6dbaab1a445e535f258795d0228
Hance08 <hancechin08@gmail.com>
2024-05-16 14:37:51 +0800

    Update MainScreen.kt

--- app/src/main/java/com/example/hw2/screens/MainScreen.kt
+++ app/src/main/java/com/example/hw2/screens/MainScreen.kt
@@ -19,5 +19,30 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun ()
+fun DetailScreen(
+    modifier: Modifier = Modifier,
+    photos: Array<Int>,
+    names: Array<String>,
+    ingredients: Array<String>,
+    itemIndex: Int?
+){
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(25.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+
+        Box(modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter = painterResource(id = photos[itemIndex!!]),
+                contentDescription = names[itemIndex],
+                modifier.clip(RoundedCornerShape(16.dp))
+            )
+        }
+        Text(text = names[itemIndex!!], fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        Text(text = ingredients[itemIndex], fontSize = 18.sp)
+    }
+}
 
