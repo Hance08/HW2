03e08fc8e7eab68fe270f52313bbd86a2e5c8424
Hance08 <hancechin08@gmail.com>
2024-05-16 16:13:04 +0800

    add

--- app/src/main/java/com/example/hw2/MainActivity.kt
+++ app/src/main/java/com/example/hw2/MainActivity.kt
@@ -7,9 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.navigation.compose.NavHost
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.hw2.screens.DetailScreen
+import com.example.hw2.screens.MainScreen
 import com.example.hw2.ui.theme.HW2Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val imageId = arrayOf(
+                        R.drawable.p1,
+                        R.drawable.p2,
+                        R.drawable.p3,
+                        R.drawable.p4,
+                        R.drawable.p5,
+                        R.drawable.p6
+                    )
+
+                    val names = arrayOf(
+                        "Peperoni",
+                        "Vegan",
+                        "FourCheese",
+                        "Margaritta",
+                        "American",
+                        "Mexican"
+                    )
+
+                    val ingredients = arrayOf(
+                        "Tomato sos, cheese, oregano, peperoni",
+                        "Tomato sos, cheese, oregano, spinach, green paprika, rukola",
+                        "Tomato sos, oregano, mozzarella, goda, parmesan, cheddar",
+                        "Tomato sos, cheese, oregano, bazillion",
+                        "Tomato sos, cheese, oregano, green paprika, red beans",
+                        "Tomato sos, cheese, oregano, corn, jalapeno, chicken",
+                    )
+
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "MainScreen") {
+                        composable(route = "MainScreen") {
+                            MainScreen(imageId, names, ingredients, navController)
+                        }
+                        composable(route = "DetailScreen/{index}",
+                            arguments = listOf(
+                                navArgument(name = "index") {
+                                    type = NavType.IntType
+                                }
+                            )
+                        ) { index->
+                            DetailScreen(
+                                photos = imageId,
+                                names = names,
+                                ingredients = ingredients,
+                                itemIndex = index.arguments?.getInt("index")
+                            )
+                        }
+                    }
+
+                }
+            }
+        }
                 }
             }
         }
